394d2d40be3375b58a62374d69a306a44e3265b1
peter <psvehla@hotmail.com>
2018-08-10 00:24:06 +1000

    Added Lombok.

--- src/main/java/com/javacodegeeks/aws/HelloWorldRequestHandler.java
+++ src/main/java/com/javacodegeeks/aws/HelloWorldRequestHandler.java
@@ -3,28 +3,26 @@
  */
 package com.javacodegeeks.aws;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import lombok.extern.log4j.Log4j2;
 
 /**
  * @author peter
  *
  */
+@Log4j2
 public class HelloWorldRequestHandler implements RequestHandler<String, String> {
-	
-	private static final Logger LOGGER = LogManager.getLogger(HelloWorldRequestHandler.class);
 
 	@Override
 	public String handleRequest(String s, Context context) {
 		
-		LOGGER.info("awsRequestId=" + context.getAwsRequestId());
-		LOGGER.info("functionName=" + context.getFunctionName());
-		LOGGER.info("functionVersion=" + context.getFunctionVersion());
-		LOGGER.info("memoryLimitInMB=" + context.getMemoryLimitInMB());
-		LOGGER.info("remainingTimeInMillis=" + context.getRemainingTimeInMillis());
+		log.info("awsRequestId=" + context.getAwsRequestId());
+		log.info("functionName=" + context.getFunctionName());
+		log.info("functionVersion=" + context.getFunctionVersion());
+		log.info("memoryLimitInMB=" + context.getMemoryLimitInMB());
+		log.info("remainingTimeInMillis=" + context.getRemainingTimeInMillis());
 		return "My first request handler:" + s;
 	}
 }
--- src/main/java/com/javacodegeeks/aws/RequestResponseHandler.java
+++ src/main/java/com/javacodegeeks/aws/RequestResponseHandler.java
@@ -6,61 +6,30 @@ package com.javacodegeeks.aws;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 
 /**
- * @author psveh
+ * @author peter
  *
  */
+@Log4j2
 public class RequestResponseHandler {
-	
-	private static final Logger LOGGER = LogManager.getLogger(RequestResponseHandler.class);
-	
+
+	@Data
 	public static class Order {
 		private String productId;
 		private String amount;
-		
-		public String getProductId() {
-			return productId;
-		}
-		
-		public void setProductId(String productId) {
-			this.productId = productId;
-		}
-		
-		public String getAmount() {
-			return amount;
-		}
-		
-		public void setAmount(String amount) {
-			this.amount = amount;
-		}
 	}
-	
+
+	@Data
 	public static class Response {
 		private String timestamp;
 		private String state;
-		
-		public String getTimestamp() {
-			return timestamp;
-		}
-		
-		public void setTimestamp(String timestamp) {
-			this.timestamp = timestamp;
-		}
-		
-		public String getState() {
-			return state;
-		}
-		
-		public void setState(String state) {
-			this.state = state;
-		}
 	}
 	
 	public Response handleRequest(Order order) {
-		LOGGER.info("Received order for product " + order.getProductId() + ": amount =" + order.getAmount());
+		log.info("Received order for product " + order.getProductId() + ": amount =" + order.getAmount());
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 		Response response = new Response();
