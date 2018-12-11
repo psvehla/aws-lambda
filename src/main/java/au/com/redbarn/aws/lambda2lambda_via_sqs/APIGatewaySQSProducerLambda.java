ceac37010b7f93bf680701ca58f1ae1f586a051e
peter <psvehla@hotmail.com>
2018-12-11 20:16:28 +0800

    Got the first version API Gateway to Lambda via SQS going.

--- src/main/java/au/com/redbarn/aws/lambda2lambda_via_sqs/APIGatewaySQSProducerLambda.java
+++ src/main/java/au/com/redbarn/aws/lambda2lambda_via_sqs/APIGatewaySQSProducerLambda.java
@@ -3,20 +3,27 @@
  */
 package au.com.redbarn.aws.lambda2lambda_via_sqs;
 
-import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
-import com.amazonaws.services.lambda.invoke.LambdaFunction;
-import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 
 import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * @author peter
  *
  */
+@Log4j2
 public class APIGatewaySQSProducerLambda {
+	// TODO: Change this to get the queue URL in a static initialiser then keep it in a static property, and see if it runs each time or not.
+	
+	public static final String queueName = "LAMBDA_TO_LAMBDA_VIA_SQS_DEMO";
 	
 	@Data
 	public static class Request {
+		// TODO: make this private
+		// TODO: change this to message
 		String name;
 	}
 	
@@ -25,17 +32,28 @@ public class APIGatewaySQSProducerLambda {
 		String message;
 	}
 	
-	public interface LambdaCalleeService {
-		@LambdaFunction(functionName="lambdaCalleeHandler")
-		Response getPersonalisedDate(Request req);
-	}
-		
-	public String handleRequest() {
-		final LambdaCalleeService lambdaCalleeService = LambdaInvokerFactory.builder().lambdaClient(AWSLambdaClientBuilder.defaultClient()).build(LambdaCalleeService.class);
-		
-		Request req = new Request();
-		req.setName("Peter");
-		
-		return lambdaCalleeService.getPersonalisedDate(req).getMessage();
+	public String handleRequest(Request req) {
+        /*
+         * Create a new instance of the builder with all defaults (credentials
+         * and region) set automatically. For more information, see
+         * Creating Service Clients in the AWS SDK for Java Developer Guide.
+         */
+        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        String theQueueUrl = "";
+        
+        // List all queues.
+        log.info("Listing all queues in your account.");
+        
+        for (final String queueUrl : sqs.listQueues().getQueueUrls()) {
+        	log.info("  QueueUrl: " + queueUrl);
+        	
+        	if (queueUrl.contains(queueName)) {
+        		theQueueUrl = queueUrl;
+        	}
+        }
+        
+        sqs.sendMessage(new SendMessageRequest(theQueueUrl, req.getName()));
+
+        return "Ok";
 	}
 }
