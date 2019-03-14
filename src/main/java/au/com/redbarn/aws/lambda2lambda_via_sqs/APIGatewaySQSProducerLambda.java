c1e5b4060652f44135191be1fdcb575d59f01222
peter <psvehla@hotmail.com>
2019-03-15 01:12:21 +0800

    Moved sqs setup out of request handler and into the class's initialisation.

--- src/main/java/au/com/redbarn/aws/lambda2lambda_via_sqs/APIGatewaySQSProducerLambda.java
+++ src/main/java/au/com/redbarn/aws/lambda2lambda_via_sqs/APIGatewaySQSProducerLambda.java
@@ -16,9 +16,24 @@ import lombok.extern.log4j.Log4j2;
  */
 @Log4j2
 public class APIGatewaySQSProducerLambda {
-	// TODO: Change this to get the queue URL in a static initialiser then keep it in a static property, and see if it runs each time or not.
+
+	private static final String queueName = "LAMBDA_TO_LAMBDA_VIA_SQS_DEMO";
+	private static final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+	private static String theQueueUrl;
 	
-	public static final String queueName = "LAMBDA_TO_LAMBDA_VIA_SQS_DEMO";
+	static {
+        // List all queues.
+        log.info("Listing all queues in your account.");
+        
+        // select the queue we're after
+        for (final String queueUrl : sqs.listQueues().getQueueUrls()) {
+        	log.info("  QueueUrl: " + queueUrl);
+        	
+        	if (queueUrl.contains(queueName)) {
+        		theQueueUrl = queueUrl;
+        	}
+        }		
+	}
 	
 	@Data
 	public static class Request {
@@ -37,24 +52,8 @@ public class APIGatewaySQSProducerLambda {
          * and region) set automatically. For more information, see
          * Creating Service Clients in the AWS SDK for Java Developer Guide.
          */
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-        String theQueueUrl = "";
-        
-        // List all queues.
-        log.info("Listing all queues in your account.");
-        
-        for (final String queueUrl : sqs.listQueues().getQueueUrls()) {
-        	log.info("  QueueUrl: " + queueUrl);
-        	
-        	if (queueUrl.contains(queueName)) {
-        		theQueueUrl = queueUrl;
-        	}
-        }
-        
         log.info("message = " + req.getMessage());
-        
         sqs.sendMessage(new SendMessageRequest(theQueueUrl, req.getMessage()));
-
         return "Ok";
 	}
 }
